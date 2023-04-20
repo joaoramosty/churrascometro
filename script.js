0036eed8442311179e4d791e462cef31209a8e69
jvramoss <devjoaoramos@gmail.com>
2023-04-20 16:40:11 -0300

    js funcional 100%

--- script.js
+++ script.js
@@ -5,20 +5,11 @@
 // // Refrigerante/água - 1000ml por pessoa + 6 horas 1500ml
 // // Crianças valem por 0,5.
 
-let inputAdultos = document.getElementById("adultos");
-let inputCriancas = document.getElementById("criancas");
-let inputDuracao = document.getElementById("duracao");
-let resultado = document.getElementById("resultado");
+const inputAdultos = document.getElementById("adultos");
+const inputCriancas = document.getElementById("criancas");
+const inputDuracao = document.getElementById("duracao");
 
-function calcular(){
-    console.log("calculando...");
-    
-    let adultos = inputAdultos.value;
-    let criancas = inputCriancas.value;
-    let duracao = inputDuracao.value;
-    let qdtTotalCarne = carnePP(duracao) * adultos + (carnePP(duracao)/2 * criancas);
-    console.log(qdtTotalCarne);
-}
+const resultado = document.getElementById("resultado");
 
 function carnePP(duracao){
     if(duracao >= 6){
@@ -27,3 +18,55 @@ function carnePP(duracao){
         return 400;
     }
 }
+
+function cervejaPP(duracao){
+    if(duracao >= 6){
+        return 2000;
+    } else {
+        return 1200;
+    }
+}
+
+function bebidasPP(duracao){
+    if(duracao >= 6){
+        return 1500;
+    } else {
+        return 1000;
+    }
+}
+
+function calcular() {
+    console.log("calculando...");
+    
+    const adultos = inputAdultos.value;
+    const criancas = inputCriancas.value;
+    const duracao = inputDuracao.value;
+    
+    const qdtTotalCarne = carnePP(duracao) * adultos + (carnePP(duracao)/2 * criancas);
+    const qdtTotalCerveja = cervejaPP(duracao) * adultos;
+    const qdtTotalBebidas = bebidasPP(duracao) * adultos + (bebidasPP(duracao)/2 * criancas);
+    
+    resultado.innerHTML = `<h2 class="result-info">Você vai precisar de:</h2>`
+  resultado.innerHTML += `
+    <div class="result-block">
+      <p>${qdtTotalCarne/1000} Kg de Carne</p>
+    </div>
+  `
+  resultado.innerHTML += `
+    <div class="result-block">
+      <p>${Math.ceil(qdtTotalCerveja/355)} latas de Cerveja</p>
+    </div>
+  `
+  resultado.innerHTML += `
+    <div class="result-block">
+      <p>${Math.ceil(qdtTotalBebidas/2000)} garrafas de Bebidas</p>
+    </div>
+    </br>
+    </br>
+    </br>
+    </br>
+    </br>
+  `
+}
+
+
